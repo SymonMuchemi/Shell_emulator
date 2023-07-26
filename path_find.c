dfd6fbb8dc2a516c6b459320648bc66f100330ea
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-27 01:44:51 +0300

    Revert "path_find.c"
    This reverts commit ebc1ebbda626a474a804ab2f34f17604fc6894ac.

--- path_find.c
+++ path_find.c
@@ -3,7 +3,7 @@
 * find_the_path - an pointer that trys to access the given path using x_OK
 * @command: the given input by user
 * @path: the array of pointers to the desired executable file
-* Return: NULL value on success
+* Return: give NULL value on success
 */
 char *find_the_path(char *command, char *path[])
 {
