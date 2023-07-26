ba53825c32f8b051a7fc90a6539a085e660a5c34
Simon-Muchemi <simonkenya846@gmail.com>
2023-07-26 23:36:17 +0300

    betty on functions.c

--- functions.c
+++ functions.c
@@ -2,26 +2,24 @@
 /**
 * _putchar - writes the character c to stdout
 * @c: The character to print
-*
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c)
 {
-if (c == '\n')
-{
-char newline = '\n';
-return (write(1, &newline, 1));
-}
-else
-{
-return (write(1, &c, 1));
-}
+	if (c == '\n')
+	{
+		char newline = '\n';
+
+		return (write(1, &newline, 1));
+	}
+	else
+		return (write(1, &c, 1));
 }
 /**
 * remove_newline - removes the new line from the buffer during execution
 * @str: character
-*
+* Return: nothing
 */
 void remove_newline(char *str)
 {
@@ -30,11 +28,8 @@ void remove_newline(char *str)
 	len = strlen(str);
 
 	if (len > 0 && str[len - 1] == '\n')
-	{
 		str[len - 1] = '\0';
-	}
 }
-
 /**
 * create_buf - Create a buffer object
 * @filename: name of file
@@ -43,32 +38,32 @@ void remove_newline(char *str)
 */
 char *create_buf(char *buf)
 {
-buf = malloc(sizeof(char) * BUFSIZE);
-if (buf == NULL)
-{
-perror("Error creating buffer!");
-exit(EXIT_FAILURE);
-}
+	buf = malloc(sizeof(char) * BUFSIZE);
+	if (buf == NULL)
+	{
+		perror("Error creating buffer!");
+		exit(EXIT_FAILURE);
+	}
 
-return (buf);
+	return (buf);
 }
 /**
 * _print_str - function that calls the _putchar
 * _putchar - writes the character c to stdout
 * @c: The character to print
-*@string: characters passed
+* @string: characters passed
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
 void _print_str(char *string)
 {
-int i = 0;
+	int i = 0;
 
-while (string[i] != '\0')
-{
-_putchar(string[i]);
-i++;
-}
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
 }
 /**
 * tokenize_args - creates an array of command-line arguments
@@ -80,45 +75,16 @@ i++;
 */
 void tokenize_args(char **args_array, char *str)
 {
-char *token, *delim;
-int i = 0;
+	char *token, *delim;
+	int i = 0;
 
-delim = " \t\n";
-token = strtok(str, delim);
+	delim = " \t\n";
+	token = strtok(str, delim);
 
-while (token && i < MAX_ARGS - 1)
-{
-args_array[i++] = token;
-token = strtok(NULL, delim);
+	while (token && i < MAX_ARGS - 1)
+	{
+		args_array[i++] = token;
+		token = strtok(NULL, delim);
+	}
+	args_array[i] = NULL;
 }
-args_array[i] = NULL;
-}
-
-/**
-* find_the_path - an pointer that trys to access the given path using x_OK
-* @command: the given input by user
-* @path: the array of pointers to the desired executable file
-* Return: give NULL value on success
-*/
-
-char *find_the_path(char *command, char *path[])
-{
-char executable_path[BUFSIZE];
-int i;
-
-if (access(command, X_OK) == 0)
-{
-return (command);
-}
-
-for (i = 0; path[i] != NULL; i++)
-{
-snprintf(executable_path, sizeof(executable_path), "%s/%s", path[i], command);
-if (access(executable_path, X_OK) == 0)
-{
-return (strdup(executable_path));
-}
-}
-return (NULL);
-}
-
